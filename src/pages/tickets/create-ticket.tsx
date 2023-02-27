d804658d6628ef57b9bdfd118e5a3c350ef9f9bc
Jonathan Kim <jonathankim980@gmail.com>
2023-02-27 01:46:44 -0600

    route bug fixed

--- src/pages/tickets/create-ticket.tsx
+++ src/pages/tickets/create-ticket.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useGetIdentity } from "@pankod/refine-core";
 import { FieldValues, useForm } from "@pankod/refine-react-hook-form";
 
